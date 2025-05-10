ab50dadeca3955ab7ac6aabc70210e15482db033
Debojyoti <debojyoti@appstango.com>
2025-05-10 10:23:34 +0530

    Initial Commit

---
+++ kindfund/kindfundApp.swift
@@ -0,0 +1,17 @@
+//
+//  kindfundApp.swift
+//  kindfund
+//
+//  Created by Debojyoti Ghosh on 10/05/25.
+//
+
+import SwiftUI
+
+@main
+struct kindfundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
